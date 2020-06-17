d408d40ccf92d0b99fa76d2812e32194e1d164f5
King, Joseph <jking@ida.org>
2020-06-17 13:59:33 -0400

    Added option for users to specify custom marginal distributions of protected class features

--- ml_mitigate_discrim.py
+++ ml_mitigate_discrim.py
@@ -1,8 +1,7 @@
-import json
+import json, warnings
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from utils import *
-
 
 class DiscriminationMitigator:
 
@@ -46,17 +45,12 @@ class DiscriminationMitigator:
                             feature_name))
             self.weights = reweights
 
-    def unadjusted_prediction(self):
-        '''
-        Estimate unadjusted fitted values using input dataframe, df
-        :return: Pandas Series
-        '''
-        return pd.DataFrame(self.model.predict(self.df), index=self.df.index)[0]
-
     def iterate_predictions(self):
         '''
-        Method iterates through all values of all protected class features, assigning on each iteration all observations
-            the value of that particular protected class.
+        Method iteratively generates N x 1 vector of predictions across all unique categorical value (K) of all
+            protected class features (C). On each iteration, all observations in self.df are (re)assigned the value Ki (i.e.
+            a particular categorical value of feature c) and using this altered dataframe the model generates an N x 1
+            vector of predictions. The final output dataframe is N x K dimensions.
         :return: Pandas dataframe
         '''
         predictions = pd.DataFrame()
@@ -64,29 +58,45 @@ class DiscriminationMitigator:
             for val in self.df[feature].unique():
                 temp = self.df.copy()
                 temp[feature] = val
-                predictions = pd.concat([predictions, pd.DataFrame(model.predict(temp),
-                                                                   index=self.df.index).rename(
+                predictions = pd.concat([predictions, pd.DataFrame(model.predict(temp),index=self.df.index).rename(
                     columns={0: feature + '_' + str(val)})], axis=1)
         return predictions
 
     def feature_marginals(self, df):
         '''
-        Generates dictionary of marginal distributions per feature in a dataframe
-        :param df: Pandas dataframe
-        :return: dictionary
+        Generates dictionary of marginal distributions per feature in a dataframe.
+        :param df: Pandas dataframe, input dataframe from which marginal distributions for each protected class feature generated.
+        :return: dictionary of marginal distributions, e.g. {'feature1': {categ1: 0.8, categ2: 0.2}, 'feature2'...} of `df`
         '''
         marginals = {}
         for feature in self.config['protected_class_features']:
             marginals[feature] = df[feature].value_counts(normalize=True, dropna=False).to_dict()
         return marginals
 
+    def check_for_onehot(self):
+        '''
+        Checks whether any protected class features in self.df are extremely correlated, suggesting one-hot vectors. Users
+            must ensure that if there is no reference category in trained model that adjacent one-hot vector marginals
+            uniquely identify observations. E.g. for two one-hot vectors, if 80% of observations for vector1=1, vector2=1
+            must be the corresponding 20%.
+        :return: nothing, a warning message if two or more features are correlated > 0.9999
+        '''
+        correlations = self.df.corr().abs().iloc[0, :]  # correlation matrix, selecting first row
+        extreme_corr = correlations[correlations > 0.9999].index.tolist()
+
+        if extreme_corr:
+            warnings.warn("\n Warning! The following features are extremely correlated and thus may be one-hot vectors: {}. \n"
+                          "If no category is omitted, users must ensure custom marginal weights for one-hot vectors align correctly.".format(' '.join(extreme_corr)))
+
     def weighted_predictions(self, marginal_dict, prediction_df):
         '''
-        Weights predictions for each feature-value by marginal distribution of that value in test set (if provided)
-            otherwise weights by marginal distribution of df supplied
+        Method weights the N x K matrix of predictions in `prediction_df` according to the marginal distribution of each
+            particular categorical value, Ki, based on dictionary of marginals. The reduces the dimension of the prediction
+            matrix down to N x C, where each column is the weighted average (from `marginal_dict`) of protected class
+            feature c.
         :param marginal_dict: dictionary of marginal distributions per feature in input dataframe
         :param prediction_df: Pandas dataframe, iterated predictions from self.iterate_predictions()
-        :return: Pandas dataframe with columns being the weighted predictions for each protected class feature
+        :return: Pandas dataframe with columns being the weighted predictions for each protected class feature, c.
         '''
         wt_pred = pd.DataFrame()
         for feature, val in marginal_dict.items():
@@ -99,24 +109,52 @@ class DiscriminationMitigator:
             wt_pred = pd.concat([wt_pred, pd.DataFrame({feature: wt})], axis=1)
         return wt_pred
 
-    def adjust_predictions(self):
+    def predictions(self):
+        '''
+        Generates predictions by calling methods of class.
+        :return: Pandas dataframe of 3 or possibly 4 columns of predictions:
+            'unadj_pred' - unadjusted predictions for self.df
+            'unif_wts' - predictions with uniform weights (i.e. simple average across N x K matrix of predictions)
+            'pop_wts' - predictions weighted to reflect the marginal distribution in the training set (if provided) otherwise self.df
+                optionally:
+                'cust_wts' - predictions with user-specified marginal weights.
+        '''
 
-        predictions = self.iterate_predictions()
+        # N x K matrix of predictions
+        iterated_predictions = self.iterate_predictions()
 
+        # Marginals for either train or df
         if self.train is not None:
             marginals = self.feature_marginals(
                 self.train)  # marginals from training set to reweight test with same composition
         else:
             marginals = self.feature_marginals(self.df)
 
-        weighted_predictions = self.weighted_predictions(marginals, predictions)
+        # Collapse down to N x C matrix of weighted predictions
+        weighted_predictions = self.weighted_predictions(marginals, iterated_predictions)
 
-        adjusted_predictions = pd.DataFrame()
-        adjusted_predictions['unif_wts'] = predictions.mean(axis=1)  # uniform weights (i.e. simple average)
-        adjusted_predictions['pop_wts'] = weighted_predictions.mean(
-            axis=1)  # weighted to match train or other marginal dist
+        # Output dataframe of adjusted final predictions
+        output_predictions = pd.DataFrame()
+        output_predictions['unadj_pred'] = pd.DataFrame(self.model.predict(self.df), index=self.df.index)[0] # unadjusted predictions
+        output_predictions['unif_wts'] = iterated_predictions.mean(axis=1)  # uniform weights (i.e. simple average)
+        output_predictions['pop_wts'] = weighted_predictions.mean(axis=1)  # weighted to match train or other marginal dist
 
-        return adjusted_predictions
+        # Dictionary of custom weights that combine user-supplied weights with marginals of either train or df
+        if self.weights is not None:
+            custom_weights = {}
+            for feature in marginals.keys():
+                if feature in self.weights.keys():
+                    custom_weights[feature] = self.weights[feature]
+                else:
+                    custom_weights[feature] = marginals[feature]
+
+            self.check_for_onehot() # check if one-hot vectors possibly present and warn
+
+            # Condense user-specified marginal weights to N x 1 vector
+            reweighted_predictions = self.weighted_predictions(custom_weights, iterated_predictions)
+            output_predictions['cust_wts'] = reweighted_predictions.mean(axis=1)
+
+        return output_predictions
 
 
 if __name__ == '__main__':
@@ -160,6 +198,5 @@ if __name__ == '__main__':
     model.compile(optimizer='adam', loss='mse')
     model.fit(X_train, y_train, epochs=80, batch_size=64, validation_data=(X_val, y_val))
 
-    dm = DiscriminationMitigator(df=X_test, model=model, config=config, train=X_train, weights=weights)
-    dm.feature_marginals(dm.df)
-    adjusted_predictions = dm.adjust_predictions()
+    dm = DiscriminationMitigator(df=X_test, model=model, config=config)
+    predictions = dm.predictions()
